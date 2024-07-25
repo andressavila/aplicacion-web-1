715175034a2027c4255cc2f5e96aab89957190b3
andressavila <davidmontenegro-b@hotmail.com>
2024-07-25 13:47:03 -0500

    j

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,16 @@ import { Component } from '@angular/core';
 })
 export default class LoginComponent {
 
+constructor(private router:Router){
+
+}
+
   titulo= 'Industrial Aceitera de Casanare';
 
   login(){
     alert("credenciales correctas");
+    this.router.navigateByUrl("dashboard");
+    
     
   }
 
